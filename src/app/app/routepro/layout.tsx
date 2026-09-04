42c6646d36eb14c1b3d772573507c19044255f75
NotaDigitalWorks <notadigitalworks@gmail.com>
2026-09-04 21:35:43 +0200

    Guard all RoutePro subroutes

---
+++ src/app/app/routepro/layout.tsx
@@ -0,0 +1,16 @@
+﻿import { redirect } from "next/navigation";
+import { userHasModuleAccess } from "@/modules/core/server/module-entitlements";
+
+export default async function RouteProLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const hasAccess = await userHasModuleAccess("routepro");
+
+  if (!hasAccess) {
+    redirect("/app/upgrade?module=routepro");
+  }
+
+  return children;
+}
